TechForce-Lyron0785/keystone

commit 644024c4dad06894ed7aa0fea8e291ad37e7cad4

Adds responsive AdminUI menu for smaller device widths (#8702)
Co-authored-by: Daniel Cousens <413395+dcousens@users.noreply.github.com>

packages/core/src/admin-ui/components/PageContainer.tsx
@@ -2,7 +2,8 @@
 /** @jsx jsx */
 
 import { jsx, useTheme } from '@keystone-ui/core';
-import { Fragment, HTMLAttributes, ReactNode } from 'react';
+import { Dispatch, Fragment, HTMLAttributes, ReactNode, SetStateAction, useState } from 'react';
+import { MenuIcon, XCircleIcon } from '@keystone-ui/icons';
 
 import { Navigation } from './Navigation';
 import { Logo } from './Logo';
@@ -25,10 +26,14 @@ const PageWrapper = (props: HTMLAttributes<HTMLElement>) => {
         css={{
           // background: colors.background,
           display: 'grid',
-          gridTemplateColumns: `minmax(300px, 1fr) 4fr`,
-          gridTemplateRows: `${HEADER_HEIGHT}px auto`,
+          gridTemplateColumns: `minmax(300px, 1fr)`,
+          gridTemplateRows: `repeat(2,${HEADER_HEIGHT}px) auto`,
           height: '100vh',
           isolation: 'isolate',
+          '@media (min-width: 576px)': {
+            gridTemplateColumns: `minmax(300px, 1fr) 4fr`,
+            gridTemplateRows: `${HEADER_HEIGHT}px auto`,
+          },
         }}
         {...props}
       />
@@ -36,19 +41,52 @@ const PageWrapper = (props: HTMLAttributes<HTMLElement>) => {
   );
 };
 
-const Sidebar = (props: HTMLAttributes<HTMLElement>) => {
+const Sidebar = ({
+  isSidebarOpen,
+  ...props
+}: HTMLAttributes<HTMLElement> & {
+  isSidebarOpen: boolean;
+  setIsSidebarOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
   // const { colors } = useTheme();
 
   return (
-    <aside
+    <div
       css={{
-        // borderRight: `1px solid ${colors.border}`,
-        minWidth: 0, // resolves collapsing issues in children
-        overflowY: 'auto',
-        WebkitOverflowScrolling: 'touch',
+        gridColumn: '1/2',
+        gridRow: '1/4',
+        display: isSidebarOpen ? 'block' : 'none',
+        height: '100vh',
+        '@media (min-width: 576px)': {
+          gridColumn: '1/2',
+          gridRow: '2/3',
+          display: 'block',
+          height: '100%',
+        },
       }}
-      {...props}
-    />
+    >
+      <div
+        onClick={() => {
+          props.setIsSidebarOpen(!isSidebarOpen);
+        }}
+        css={{
+          float: 'right',
+          padding: '10px',
+          display: 'block',
+          '@media (min-width: 576px)': { display: 'none' },
+        }}
+      >
+        <XCircleIcon />
+      </div>
+      <aside
+        css={{
+          // borderRight: `1px solid ${colors.border}`,
+          minWidth: 0, // resolves collapsing issues in children
+          WebkitOverflowScrolling: 'touch',
+        }}
+        {...props}
+      />
+    </div>
   );
 };
 
@@ -74,6 +112,7 @@ const Content = (props: HTMLAttributes<HTMLElement>) => {
 
 export const PageContainer = ({ children, header, title }: PageContainerProps) => {
   const { colors, spacing } = useTheme();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
     <PageWrapper>
       <div
@@ -88,6 +127,14 @@ export const PageContainer = ({ children, header, title }: PageContainerProps) =
         }}
       >
         <Logo />
+        <div
+          onClick={() => {
+            setIsSidebarOpen(!isSidebarOpen);
+          }}
+          css={{ display: 'block', '@media (min-width: 576px)': { display: 'none' } }}
+        >
+          <MenuIcon />
+        </div>
       </div>
       <header
         css={{
@@ -102,9 +149,10 @@ export const PageContainer = ({ children, header, title }: PageContainerProps) =
         }}
       >
         <title>{title ? `Keystone - ${title}` : 'Keystone'}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {header}
       </header>
-      <Sidebar>
+      <Sidebar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen}>
         <Navigation />
       </Sidebar>
       <Content>{children}</Content>

packages/core/src/___internal-do-not-use-will-break-in-patch/admin-ui/pages/ItemPage/common.tsx
@@ -78,8 +78,12 @@ export function ColumnLayout(props: HTMLAttributes<HTMLDivElement>) {
         css={{
           alignItems: 'start',
           display: 'grid',
-          gap: spacing.xlarge,
-          gridTemplateColumns: `2fr 1fr`,
+          gap: spacing.none,
+          gridTemplateColumns: `100vw`,
+          '@media (min-width: 576px)': {
+            gridTemplateColumns: `2fr 1fr`,
+            gap: spacing.xlarge,
+          },
         }}
         {...props}
       />

packages/fields-document/src/DocumentEditor/primitives/toolbar.tsx
@@ -58,6 +58,7 @@ export const ToolbarGroup = forwardRefWithAs<'div', ToolbarGroupProps>(
             justifyContent: 'start',
             alignItems: directionToAlignment[direction],
             height: '100%',
+            overflowX: 'auto',
           }}
           {...props}
         >

commit 73bbb1f6a078c8e9b63e546b2c52088bda9295cc

Fixes AdminUI overflow bug (#8754) introduced in #8702

packages/core/src/admin-ui/components/CellContainer.tsx
@@ -1,7 +1,7 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 
-import { ReactNode } from 'react';
+import type { ReactNode } from 'react';
 import { jsx, useTheme } from '@keystone-ui/core';
 
 /**

packages/core/src/admin-ui/components/CreateButtonLink.tsx
@@ -2,10 +2,10 @@
 /** @jsx jsx */
 import { Button } from '@keystone-ui/button';
 import { jsx } from '@keystone-ui/core';
-import { ListMeta } from '../../types';
+import type { ListMeta } from '../../types';
 import { Link } from '../router';
 
-export const CreateButtonLink = (props: { list: ListMeta }) => {
+export function CreateButtonLink (props: { list: ListMeta }) {
   return (
     <Button
       css={{
@@ -23,4 +23,4 @@ export const CreateButtonLink = (props: { list: ListMeta }) => {
       Create {props.list.singular}
     </Button>
   );
-};
+}

packages/core/src/admin-ui/components/PageContainer.tsx
@@ -2,7 +2,7 @@
 /** @jsx jsx */
 
 import { jsx, useTheme } from '@keystone-ui/core';
-import { Dispatch, Fragment, HTMLAttributes, ReactNode, SetStateAction, useState } from 'react';
+import { Fragment, HTMLAttributes, ReactNode, useState } from 'react';
 import { MenuIcon, XCircleIcon } from '@keystone-ui/icons';
 
 import { Navigation } from './Navigation';
@@ -46,7 +46,6 @@ const Sidebar = ({
   ...props
 }: HTMLAttributes<HTMLElement> & {
   isSidebarOpen: boolean;
-  setIsSidebarOpen: Dispatch<SetStateAction<boolean>>;
 }) => {
   // const { colors } = useTheme();
 
@@ -54,7 +53,7 @@ const Sidebar = ({
     <div
       css={{
         gridColumn: '1/2',
-        gridRow: '1/4',
+        gridRow: '2/4',
         display: isSidebarOpen ? 'block' : 'none',
         height: '100vh',
         '@media (min-width: 576px)': {
@@ -63,21 +62,10 @@ const Sidebar = ({
           display: 'block',
           height: '100%',
         },
+        overflowY: 'auto',
+        WebkitOverflowScrolling: 'touch',
       }}
     >
-      <div
-        onClick={() => {
-          props.setIsSidebarOpen(!isSidebarOpen);
-        }}
-        css={{
-          float: 'right',
-          padding: '10px',
-          display: 'block',
-          '@media (min-width: 576px)': { display: 'none' },
-        }}
-      >
-        <XCircleIcon />
-      </div>
       <aside
         css={{
           // borderRight: `1px solid ${colors.border}`,
@@ -133,7 +121,7 @@ export const PageContainer = ({ children, header, title }: PageContainerProps) =
           }}
           css={{ display: 'block', '@media (min-width: 576px)': { display: 'none' } }}
         >
-          <MenuIcon />
+          {isSidebarOpen ? <XCircleIcon /> : <MenuIcon />}
         </div>
       </div>
       <header
@@ -146,13 +134,14 @@ export const PageContainer = ({ children, header, title }: PageContainerProps) =
           minWidth: 0, // fix flex text truncation
           paddingLeft: spacing.xlarge,
           paddingRight: spacing.xlarge,
+          visibility: isSidebarOpen ? 'hidden' : 'visible',
         }}
       >
         <title>{title ? `Keystone - ${title}` : 'Keystone'}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {header}
       </header>
-      <Sidebar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen}>
+      <Sidebar isSidebarOpen={isSidebarOpen}>
         <Navigation />
       </Sidebar>
       <Content>{children}</Content>